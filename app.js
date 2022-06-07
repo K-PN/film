358c7ddace3adb1a267d9000142607f3dbdc6f3b
KP <ngockhanh1116@gmail.com>
2022-06-07 20:42:19 +0700

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+$(".menu-chinh").click(function () {
+        //Khi click vào Assistive touch, nếu các menu con đang ẩn thì hiện lên
+    if ($(".menu-list").css('display') == 'none') $(".menu-list").show(200, 'swing');
+        // đang hiện thì ẩn đi
+    else $(".menu-list").fadeOut('fast');
+});
+//click vào 1 trong các menu con thì tất cả cùng ẩn đi
+// thiết kế chức năng do quản trị viên code, có thể tham khảo trên 123itvn.com
+$(".menu-1,.menu-2,.menu-3,.menu-4,.menu-5,.menu-6").click(function () {
+$(".menu-list").fadeOut('fast');
+});
+    // code cho nút lên đầu trang - backtotop
+$(".menu-1").click(function () {
+    $("body,html").animate({ scrollTop: 0 }, "slow");
+});
 $(document).ready(() => {
     $('#hamburger-menu').click(() => {
         $('#hamburger-menu').toggleClass('active')
